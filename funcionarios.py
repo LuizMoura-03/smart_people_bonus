b5d1b0605407dd77223b6e16b3ee1454140c208e
LuizFernando <barbosa.luiz@zup.com.br>
2025-04-22 16:31:07 -0300

    :sparkles: feat: adiciona classe Gerente com calculo de bonus

--- funcionarios.py
+++ funcionarios.py
@@ -19,7 +19,17 @@ class Funcionario(ABC):
         if salario < 0:
             raise ValueError("Salário não pode ser negativo")
         self._salario = salario
+   
         
 class FuncionarioComum(Funcionario):
     def calcular_bonus(self):
-        return self._salario * 0.10      
+        return self._salario * 0.10 
+    
+  
+class Gerente(Funcionario):
+    def __init__(self, nome, salario, bonus_adicional):
+        super().__init__(nome, salario)
+        self.bonus_adicional = bonus_adicional
+
+    def calcular_bonus(self):
+        return self._salario * 0.20 + self.bonus_adicional
