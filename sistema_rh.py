cd30c5b7fe2f89dc9b13fbb792b3187dfa8246ad
LuizFernando <barbosa.luiz@zup.com.br>
2025-04-22 19:08:46 -0300

    :sparkles: feat: adiciona classe SistemaRH com o metodo mostrar_bonus decorado

--- sistema_rh.py
+++ sistema_rh.py
@@ -3,3 +3,8 @@ def logar_acao(func):
         print("Log: Ação de bônus executada")
         return func(funcionario)
     return wrapper
+
+class SistemaRH:
+    @logar_acao
+    def mostrar_bonus(self, funcionario):
+        print(f"Funcionário: {funcionario.get_nome()}, Bônus: {funcionario.calcular_bonus()}")
