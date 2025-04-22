7dfdb6ab960c0473a16b2ee51a719222917510ae
LuizFernando <barbosa.luiz@zup.com.br>
2025-04-22 19:52:47 -0300

    :test_tube: test: Adiciona testes para verificar log e saida de mostrar_bonus, implementa mock para funcao print

--- testes.py
+++ testes.py
@@ -1,5 +1,7 @@
 import unittest
+from unittest.mock import patch
 from funcionarios import FuncionarioComum, Gerente
+from sistema_rh import SistemaRH
 
 class TestFuncionarios(unittest.TestCase):
     def test_calculo_bonus_funcionario_comum(self):
@@ -19,6 +21,14 @@ class TestFuncionarios(unittest.TestCase):
         gerente = Gerente("Luiz", 4000, 1000)
         with self.assertRaises(ValueError):
             gerente.set_salario(-4000)
+            
+@patch('builtins.print')
+def test_mostrar_bonus(self, mock_print):
+        funcionario = FuncionarioComum("João", 2000)
+        sistema = SistemaRH()
+        sistema.mostrar_bonus(funcionario)
+        mock_print.assert_any_call("Log: Ação de bônus executada")
+        mock_print.assert_any_call("Funcionário: João, Bônus: 200")         
 
 if __name__ == '__main__':
     unittest.main()
